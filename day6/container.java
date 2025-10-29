23b8d796550eb745a0fb3e39062e68605d84e288
chidera peter <ronty@pc.local>
2025-10-29 11:36:11 +0100

    first commit

---
+++ day6/container.java
@@ -0,0 +1,16 @@
+class Container{
+      public static void main(String[] args){
+        // variable decleration or variable initialization
+        int intvalue=10;
+        long longvalue=1063783939873654636l;
+        char charvalue='A';
+        String stringvalue="hello world";
+        boolean  booleanvalue=true; 
+        System.out.println(intvalue);
+        System.out.println(longvalue);
+        System.out.println(charvalue);
+        System.out.println(stringvalue);
+        System.out.println(booleanvalue);
+      } 
+
+}
